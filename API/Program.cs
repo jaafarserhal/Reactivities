edde151468ebfde1b8685d03db5e5d2b193da7ec
Jaafar Serhal <jaafar.serhal@yuvo.net>
2022-05-19 15:48:06 +0300

    End of section 2

---
+++ API/Program.cs
@@ -0,0 +1,52 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+// Add services to the container.
+IConfiguration _config = builder.Configuration;
+IWebHostEnvironment environment = builder.Environment;
+
+builder.Services.AddDbContext<DataContext>(opt =>
+        opt.UseSqlServer(
+            _config.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+using var scope = app.Services.CreateScope();
+
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedData(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "Error occured during migration");
+}
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+//app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+ await app.RunAsync();
