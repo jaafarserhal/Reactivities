e48129be59de1eabe1a21a54afa1914713ffb088
Jaafar Serhal <jaafar.serhal@yuvo.net>
2022-05-27 16:07:31 +0300

    End of Section 8

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,7 +1,6 @@
 import {makeAutoObservable, runInAction} from 'mobx';
 import agent from '../api/agent';
 import { Activity } from '../models/activity';
-import {v4 as uuid} from 'uuid';
 export default class ActivityStore {
 
     activityRegistry = new Map<string, Activity>();
@@ -19,17 +18,50 @@ export default class ActivityStore {
         Date.parse(a.date) - Date.parse(b.date));
     }
     loadActivities = async () => {
+        this.loadingInitial = true;
         try {
             const activities = await agent.Activities.list();
             activities.forEach(activity => {
-                activity.date = activity.date.split('T')[0];
-                this.activityRegistry.set(activity.id,activity);
+                this.setActivity(activity);
             })
             this.setLoadingInitial(false);
         } catch (error) {
             console.log(error);
             this.setLoadingInitial(false);
         }
+    }
+
+    loadActivity = async (id: string) => {
+        let activity = this.getActivity(id);
+        if(activity) {
+            runInAction(() => {
+            this.selectedActivity = activity;
+        });
+            return activity;
+        }else{
+            this.loadingInitial = true;
+            try {
+                activity = await agent.Activities.details(id);
+                runInAction(() => {
+                    this.selectedActivity = activity;
+                });
+                this.setActivity(activity);
+                this.setLoadingInitial(false);
+                return activity;
+            } catch (error) {
+                console.log(error);
+                this.setLoadingInitial(false);
+            }
+        }
+    }
+
+    private setActivity = (activity: Activity )=> {
+        activity.date = activity.date.split('T')[0];
+        this.activityRegistry.set(activity.id,activity);
+    }
+
+    private getActivity = (id: string)=> {
+        return this.activityRegistry.get(id);
     }
 
     setLoadingInitial = (state: boolean) => {
@@ -40,26 +72,8 @@ export default class ActivityStore {
         this.editMode = state;
     }
 
-    selectActivity = (id: string) => {
-        this.selectedActivity = this.activityRegistry.get(id);
-    }
-    
-    cancelSelectedActivity = () => {
-        this.selectedActivity = undefined;
-    }
-
-    openForm = (id?: string) => {
-        id ? this.selectActivity(id): this.cancelSelectedActivity();
-        this.editMode = true;
-    }
-
-    closeForm = () => {
-        this.editMode = false;
-    }
-
     createActivity = async (activity:Activity) => {
         this.loading = true;
-        activity.id = uuid();
         try {
             await agent.Activities.create(activity);
           runInAction(()=>{
@@ -99,10 +113,7 @@ export default class ActivityStore {
         try {
             await agent.Activities.delete(id);
             runInAction(()=>{ 
-                this.activityRegistry.delete(id);
-               
-                if (this.selectedActivity?.id === id) this.cancelSelectedActivity();
-                
+                this.activityRegistry.delete(id);                
                 this.loading = false;
             })
         } catch (error) {
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,31 +1,53 @@
 import { observer } from 'mobx-react-lite';
-import React,{useState,ChangeEvent} from 'react';
+import React,{useState,ChangeEvent, useEffect} from 'react';
+import { useParams,useNavigate, Link  } from 'react-router-dom';
 import { Segment,Form, Button } from 'semantic-ui-react';
+import LoadingComponent from '../../../app/layout/LoadingComponent';
 import { useStore } from '../../../app/stores/store';
+import {v4 as uuid} from 'uuid';
 
 
 export default observer(function ActivityForm() {
     const {activityStore} = useStore();
-    const {selectedActivity,closeForm,createActivity,updateActivity,loading} = activityStore;
-    const intialState = selectedActivity ??  {
-      id: '',
-      title:  '',
-      date:  '',
-      descriptio: '',
-      category: '',
-      city:  '',
-      venue:  '',
-    }
-    const [activity,setActivity] = useState(intialState);
+    const {loadActivity,createActivity,updateActivity,loading,loadingInitial} = activityStore;
+    const {id} = useParams<{id: string}>();
+    const navigate = useNavigate();
+    const [activity,setActivity] = useState({
+        id: '',
+        title:  '',
+        date:  '',
+        descriptio: '',
+        category: '',
+        city:  '',
+        venue:  '',
+    });
+
+
+   useEffect(()=> {
+       if (id)  loadActivity(id).then(activity => setActivity(activity!))
+   },[id,loadActivity])
 
     function handleSubmit() {
-      activity.id ? updateActivity(activity) : createActivity(activity);
+      if(activity.id.length === 0) {
+          let newActivity = {
+              ...activity,id:uuid()
+          };
+          createActivity(newActivity).then(() => {
+            navigate(`/activities/${newActivity.id}`)
+          });
+      } else{
+        updateActivity(activity).then(() => {
+            navigate(`/activities/${activity.id}`)
+          });
+      }
     }
 
     function handleInputChange(event:ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
         const {name,value} = event.target;
         setActivity({...activity,[name]:value})
     }
+
+    if(loadingInitial) return <LoadingComponent content='Loading activity...' />
     return(
         <Segment clearing>
             <Form onSubmit={handleSubmit} autoComplete = 'off'>
@@ -35,8 +57,8 @@ export default observer(function ActivityForm() {
                 <Form.Input type="date" placeholder = 'Date' value = {activity.date} name="date" onChange = {handleInputChange}/>
                 <Form.Input placeholder = 'City' value = {activity.city} name="city" onChange = {handleInputChange}/>
                 <Form.Input placeholder = 'Venue' value = {activity.venue} name="venue" onChange = {handleInputChange}/>
-                <Button loading={loading} floated='right' positive type='submit' content= 'Submit'/>
-                <Button onClick={closeForm} floated='right' type='button' content= 'Cancel'/>
+                <Button  loading={loading} floated='right' positive type='submit' content= 'Submit'/>
+                <Button  as={Link} to='/activities' floated='right' type='button' content= 'Cancel'/>
             </Form>
         </Segment>
     )
